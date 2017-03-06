62b4bd70c2bdc15be114598758d7c8abf50d5751
David Lu <david.lu97@outlook.com>
2017-03-05 16:56:37 -0800

    mentee form

--- client/components/MenteeIntakeForm.jsx
+++ client/components/MenteeIntakeForm.jsx
@@ -4,7 +4,7 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 export default class MenteeIntakeForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {response: ''};
+    this.state = {goals: []};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,10 +13,13 @@ export default class MenteeIntakeForm extends React.Component {
     this.getArrayOfConsecutiveInts = this.getArrayOfConsecutiveInts.bind(this);
     this.renderPersonalInformation = this.renderPersonalInformation.bind(this);
     this.renderSelectInput = this.renderSelectInput.bind(this);
-    this.renderGoals = this.renderSkills.bind(this);
+    this.renderGoals = this.renderGoals.bind(this);
     this.renderCheckBox = this.renderCheckBox.bind(this);
-    this.renderEmploymentHistory = this.renderEmploymentHistory.bind(this);
   }
+
+  static contextTypes = {
+    router: React.PropTypes.object,
+  };
 
   handleChange(event) {
     const target = event.target;
@@ -45,20 +48,12 @@ export default class MenteeIntakeForm extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log(events.target.value);
+    console.log(this.state);
   }
 
   renderOptions(value) {
     return (
       <option key={value}>{value}</option>
-    );
-  }
-
-  renderCheckBox(displayName, fieldName) {
-    return (
-      <div>
-        <Input type="checkbox" name={fieldName} onChange={this.handleCheckboxChange} />{displayName}
-      </div>
     );
   }
 
@@ -83,21 +78,9 @@ export default class MenteeIntakeForm extends React.Component {
         <Label>
           {displayName + ':'}
           <Input name={fieldName} type="select" value={this.state[fieldName]} onChange={this.handleChange}>
+            <option key="empty"></option>
             {options.map(this.renderOptions)}
           </Input>
-        </Label>
-      </FormGroup>
-    );
-  }
-
-  renderCheckboxInput(displayName, fieldName, fields) {
-    return (
-      <FormGroup key={fieldName}>
-        <Label>
-          {displayName + ':'}
-          {fields.map((field) => {
-            this.renderCheckboxField(fieldName, field);
-          })}
         </Label>
       </FormGroup>
     );
@@ -110,32 +93,30 @@ export default class MenteeIntakeForm extends React.Component {
         <FormGroup key="name">
           <Label>
             First Name:
-            <Input name="firstname" type="text" value={this.state.firstname} onChange={this.handleChange}/>
+            <Input name="firstName" type="text" value={this.state.firstName} onChange={this.handleChange}/>
           </Label>
           <Label>
             Last Name:
-            <Input name="lastname" type="text" value={this.state.lastname} onChange={this.handleChange}/>
+            <Input name="lastName" type="text" value={this.state.lastName} onChange={this.handleChange}/>
           </Label>
         </FormGroup>
         <Label>
           Date of Birth(Month-Day-Year):
         </Label>
-        <FormGroup key="dob" row style={{marginLeft: 0}}>
-          <Input name="month" className="col-sm-1" type="select" value={this.state.month} onChange={this.handleChange}>
-            {this.getArrayOfConsecutiveInts(12).map(this.renderOptions)}
-          </Input>
-          <Input name="day" className="col-sm-1" type="select" value={this.state.day} onChange={this.handleChange}>
-            {this.getArrayOfConsecutiveInts(31).map(this.renderOptions)}
-          </Input>
-          <Input name="year" className="col-sm-2" type="select" value={this.state.year} onChange={this.handleChange}>
-            {this.getArrayOfConsecutiveInts(70).map(n => 2018 - n).map(this.renderOptions)}
-          </Input>
+        <FormGroup key="dateOfBirth" row style={{marginLeft: 0}}>
+          <Input name="dateOfBirth" type="date" value={this.state.dateOfBirth} onChange={this.handleChange} />
         </FormGroup>
         {this.renderTextInput('Phone Number', 'phone')}
         {this.renderTextInput('Email', 'email')}
         {this.renderTextInput('Mailing Address', 'address')}
-        {this.renderSelectInput('Program', 'program', ['Bridge', 'Residential Shelter'])}
-        {this.renderTextInput('Case Manager (residential only)', 'caseManager')}
+      </div>
+    );
+  }
+
+  renderCheckBox(displayName, fieldName) {
+    return (
+      <div>
+        <Input type="checkbox" name={fieldName} onChange={this.handleCheckboxChange} />{displayName}
       </div>
     );
   }
@@ -143,28 +124,31 @@ export default class MenteeIntakeForm extends React.Component {
   renderGoals() {
     return (
       <div>
-        <h1>Goals</h1>
         <h3>Please indicate the reasons that you are seeking Career Development Services</h3>
-        {this.renderCheckBox('Acquire full-time employment', 'full-time')}
-        {this.renderCheckBox('Acquire part-time employment', 'part-time')}
-        {this.renderCheckBox('Increase income/hours', 'income')}
-        {this.renderCheckBox('Change career path', 'career')}
-        {this.renderCheckBox('Enroll in 2 or 4 year University Program', 'enroll-university')}
-        {this.renderCheckBox('Enroll in technical degree or certification program', 'enroll-technical')}
-        {this.renderCheckBox('Overcome barrier to employment (Acquire GED, computer skills, English tutoring, interview skills, professional attire, resume building, criminal record expungement, improving credit)', 'barrier')}
-        {this.renderCheckBox('Other (please specify)', 'other')}
-        {this.renderTextInput('If you chose other, please specify', 'other')}
+        <FormGroup key="goals" style={{marginLeft: 20}}>
+          {this.renderCheckBox('Acquire full-time employment', 'full-time')}
+          {this.renderCheckBox('Acquire part-time employment', 'part-time')}
+          {this.renderCheckBox('Increase income/hours', 'income')}
+          {this.renderCheckBox('Change career path', 'career')}
+          {this.renderCheckBox('Enroll in 2 or 4 year University Program', 'enroll-university')}
+          {this.renderCheckBox('Enroll in technical degree or certification program', 'enroll-technical')}
+          {this.renderCheckBox('Overcome barrier to employment (Acquire GED, computer skills, English tutoring, interview skills, professional attire, resume building, criminal record expungement, improving credit)', 'barrier')}
+          {this.renderCheckBox('Other (please specify)', 'other')}
+          {this.renderTextInput('If you chose other, please specify', 'other')}
+        </FormGroup>
       </div>
     );
   }
 
   render() {
+    console.log('updated');
     return (
       <div className="container">
         <h1>Personal Information</h1>
         <Form onSubmit={this.handleSubmit}>
           {this.renderPersonalInformation()}
           {this.renderGoals()}
+          <Button onSubmit={this.handleSubmit}>Submit</Button>
         </Form>
       </div>
     );
